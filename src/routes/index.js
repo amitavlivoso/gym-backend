f27e3ae7d8f2e40decae06ea4344e6a5d2cfd265
amitavlivoso <amitav@livosotech.com>
2025-06-24 18:14:25 +0530

    .

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 
 router.use("/:tableName", require("./query.route"));
 router.use("/auth", require("./auth.route"));
-// router.use("/pay", require("./razorpay.route"));
+router.use("/pay", require("./razorpay.route"));
 
 module.exports = router;
